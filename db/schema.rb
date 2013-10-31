f726d2273a13c896492061a3ce90f9af4fc6a207
Luke Macauley <luke@lukemacauley.com>
2013-10-31 15:38:27 +0000

    Devise Confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 20131031152359) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
