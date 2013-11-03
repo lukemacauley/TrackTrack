d675417826acffdfcb8a624ca03dcb8dfa388c01
Luke Macauley <luke@lukemacauley.com>
2013-11-03 23:23:10 +0000

    Remove red on focus and add pulsing border, update arrow colour

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
+
+	validates :name, presence: true
+	
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, #:async,
          :recoverable, :rememberable, :trackable, :validatable
 
 	has_many :meals
-
-	validates :name, presence: true
 
 end
